08afa40d2c4c070fdbfa7f8a817d9b033e32223c
Ryan Dalal <ryanadalal@gmail.com>
2024-11-07 14:14:34 +0700

    working post requests

--- index.mjs
+++ index.mjs
@@ -1,11 +1,20 @@
 import express from "express";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const port = process.env.PORT;;
 
 app.get('/', (req, res) => {
-    res.send("Test");
+    res.sendFile(__dirname + '/index.html');
 });
+
+app.post('/', (req, res) => {
+    console.log("testpost");
+})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
