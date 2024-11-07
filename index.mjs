751b6a0e9e518a22e894184e7379d6110b43d56b
Ryan Dalal <ryanadalal@gmail.com>
2024-11-07 14:56:58 +0700

    started with firebase

--- index.mjs
+++ index.mjs
@@ -1,12 +1,40 @@
 import express from "express";
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import dotenv from 'dotenv';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, get, child, set, onValue} from 'firebase/database';
+import { getFirestore, collection, getDoc, doc} from 'firebase/firestore';
 
+// set the default directory
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// configure enviornment variables
+dotenv.config();
+const {
+  PORT,
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+} = process.env;
+
+// initialize firebase
+const firebase = initializeApp({
+    apiKey: API_KEY,
+    authDomain: AUTH_DOMAIN,
+    projectId: PROJECT_ID,
+    storageBucket: STORAGE_BUCKET,
+    messagingSenderId: MESSAGING_SENDER_ID,
+    appId: APP_ID,
+  },
+);
+console.log(getDatabase());
+
 const app = express();
-const port = process.env.PORT;;
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -16,6 +44,7 @@ app.post('/', (req, res) => {
     console.log("testpost");
 })
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
+
